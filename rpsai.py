a7a08b46fb50414913e6d3d984254a9611aed5df
madesh405 <madesh6405@gmail.com>
2024-09-25 19:12:24 +0530

    still wrong

--- rpsai.py
+++ rpsai.py
@@ -1,6 +1,5 @@
 from random import choice
 
-hist={}
 weapons=["rock","paper","scissor"]
 
 def computer_choice(hist:dict,weapons:list[str]) -> str:
@@ -11,9 +10,10 @@ def computer_choice(hist:dict,weapons:list[str]) -> str:
         return choice(weapons)
 
     else:
-        for x,y in hist:
-            if y==hist[len(hist)]:
-                all.append(hist[x-1])
+        for x in hist:
+            if hist[x]==hist[len(hist)]:
+                if x-1!=0:
+                    all.append(hist[x-1])
 
         rt=all.count(weapons[0])
         pt=all.count(weapons[1])
@@ -23,7 +23,11 @@ def computer_choice(hist:dict,weapons:list[str]) -> str:
         sp=st/len(hist)
         pdict={rp:"rock",pp:"paper",sp:"scissor"} #have to change this part error in rare cases
         m=max(pdict)
-        return pdict[m]
+        ind=weapons.index(pdict[m])
+        if ind==2:
+            return weapons[0]
+        else:
+            return weapons[ind+1]
 
 def player_choice(weapons:list) -> str:       #the players's choice
     print("\n\nwelcome to rock paper scissor game")
@@ -66,17 +70,20 @@ def rules(crps:str,prps:str,weapons:list) -> int:
 
 
 #__main__
-
+hist={}
 score=[0,0]
 game=True
+t=1
 while game:
     crps=computer_choice(hist,weapons)
     prps=player_choice(weapons)
+    hist[t]=prps
     print("cmputer:",crps,"player:",prps,"\n\n")
     curscore=rules(crps,prps,weapons)
     score=[curscore[0]+score[0],curscore[1]+score[1]]
     cont=input("wanna continue? (y,n) ")
     if cont=="n":
+        t+=1
         break
 
 print("the scores are computer",score[0],"player",score[1])
